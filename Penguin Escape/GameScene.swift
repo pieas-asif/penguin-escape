3650dcf46afd786a2040dd6a467ca0ddd418decf
Shaman Sharif <pieas.asif4@gmail.com>
2021-10-14 23:26:54 +0600

    SK Actions

--- Penguin Escape/GameScene.swift
+++ Penguin Escape/GameScene.swift
@@ -3,6 +3,13 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
-        // todo: something
+        let mySprite = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 50, height: 50))
+        mySprite.position = CGPoint(x: 300, y: 300)
+        self.addChild(mySprite)
+        let demoAction1 = SKAction.scale(by: 4, duration: 5)
+        let demoAction2 = SKAction.rotate(byAngle: 5, duration: 5)
+        let demoAction3 = SKAction.moveBy(x: 100, y: 200, duration: 3)
+        let actionGroup = SKAction.sequence([demoAction1, demoAction2, demoAction3])
+        mySprite.run(actionGroup)
     }
 }
