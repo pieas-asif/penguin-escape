d0b0c61274193c4a20872ffed50816d91553eb7f
Shaman Sharif <pieas.asif4@gmail.com>
2021-10-16 07:14:43 +0600

    Sequence

--- Penguin Escape/GameScene.swift
+++ Penguin Escape/GameScene.swift
@@ -8,8 +8,11 @@ class GameScene: SKScene {
         self.addChild(mySprite)
         let demoAction1 = SKAction.scale(by: 4, duration: 5)
         let demoAction2 = SKAction.rotate(byAngle: 5, duration: 5)
-        let demoAction3 = SKAction.moveBy(x: 100, y: 200, duration: 3)
-        let actionGroup = SKAction.sequence([demoAction1, demoAction2, demoAction3])
-        mySprite.run(actionGroup)
+        let demoAction4 = SKAction.scaleX(to: 8, duration: 5)
+        let demoAction3 = SKAction.moveBy(x: 100, y: 50, duration: 3)
+        
+        let actionGroup = SKAction.group([demoAction4, demoAction2]);
+        let actionSequence = SKAction.sequence([demoAction1, actionGroup, demoAction3])
+        mySprite.run(actionSequence)
     }
 }
